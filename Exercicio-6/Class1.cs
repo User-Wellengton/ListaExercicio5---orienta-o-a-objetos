ed28d88d88a607726555cb35cc615133a0d2e3d3
User-Wellengton <wellengtonjm@gmail.com>
2022-03-08 17:06:22 -0300

    ...

---
+++ Exercicio-6/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_6
+{
+    internal class converterCparaF
+    {
+
+        // objeto *** caracteristicas 
+        public double tempF;
+        public double tempC;
+
+        //metodo
+        public double converterParaF()
+        {
+
+            tempF = (tempC / 1.8)  + 32;
+
+            return tempF;
+
+        }
+    }
+}
---
+++ Exercicio-6/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Exercicio_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // (classe/ molde ) (objeto)
+            converterCparaF converter = new converterCparaF();
+
+
+            Console.WriteLine("Insira a temperatura desejada em Fahrenheit:");
+            converter.tempC = double.Parse(Console.ReadLine());
+
+
+            // chamando a classe 
+            converter.converterParaF();
+
+
+            // chamar RESULTADO esta recebendo 
+            double resultado = converter.converterParaF();
+
+
+            Console.WriteLine(" A temperatura apresentada em Fahrenheit é de :" + converter.tempC + " °C");
+            Console.WriteLine("A temperatuda convertida para Celsius é de: " + resultado + " °F");
+
+            Console.ReadLine();
+
+
+
+
+        }
+    }
+}
